9fb2c132e82497186ceefe0bc6f7882f3e877f1c
Weston Harper <wes@tao.team>
2019-04-08 11:37:45 -0600

    added new getDigits solution

--- algorithms/chapter4/getDigits.js
+++ algorithms/chapter4/getDigits.js
@@ -26,4 +26,24 @@ function getDigits2(str) {
 
 console.log(getDigits2(testStr));
 
+function getDigits3(str) {
+  // associate each string with its digit value
+  var numberMap = { "0": 0, "1": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9 }
+  // digitPlace represents "ones", "tens", "hundreds", etc. 
+  // in other words, where should the digit go?
+  var digitPlace = 1;
+  var resultNum = 0;
+  // loop through the input string backward
+  for (var i = str.length - 1; i >= 0; i--) {
+    // use the quick lookup speed of the object to check for the existence of a key
+    if(numberMap[str[i]] !== undefined) {
+      // if key exists, add to result in the proper "place"
+      resultNum += numberMap[str[i]] * digitPlace;
+      digitPlace *= 10;
+    }
+  }
+  return resultNum;
+}
+console.log(getDigits3("0s1a3y5w7h9a2t4?6!8?0"))
+
 // console.log(parseInt("h") === NaN);
