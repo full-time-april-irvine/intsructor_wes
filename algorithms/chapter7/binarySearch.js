a7b4b6749554738fad83015117c89fe71aa5add7
Weston Harper <wes@tao.team>
2019-05-09 15:20:45 -0600

    added binary search and additional tests

--- algorithms/chapter7/binarySearch.js
+++ algorithms/chapter7/binarySearch.js
@@ -1,7 +1,27 @@
 function binarySearch(arr, val) {
-  
+  if(arr === undefined || val === undefined) {
+    return false;
+  }
+
+  let leftBound = 0;
+  let rightBound = arr.length - 1;
+  if(arr[leftBound] === val || arr[rightBound] === val) {
+    return true;
+  }
+
+  while(leftBound < rightBound - 1) {
+    let mid = Math.trunc(((rightBound - leftBound) / 2) + leftBound);
+    if(arr[mid] < val) {
+      leftBound = mid;
+    } else if(arr[mid] > val) {
+      rightBound = mid;
+    } else {
+      return true;
+    }
+  }
+  return false;
 }
 
 module.exports = {
-  binarySearch: binarySearch
+  binarySearch,
 }
--- algorithms/chapter7/spec/binarySearch.spec.js
+++ algorithms/chapter7/spec/binarySearch.spec.js
@@ -1,19 +1,36 @@
-var binarySearch = require('../binarySearch').binarySearch;
+const binarySearch = require('../binarySearch').binarySearch
 
 describe('`binarySearch()`', function() {
-  it('returns true if val is in the given array', function() {
+  test('returns true if val is in the given array and the given array has even length', function() {
     expect(binarySearch([1, 2, 3, 4, 5, 6], 4)).toBe(true);
   });
 
-  it('returns false if val is not in the given array', function() {
+  test('returns true if val is in the given array and the given array has odd length', function() {
+    expect(binarySearch([1, 2, 3, 4, 5], 4)).toBe(true);
+  });
+
+  test('returns true if val is in the given array and the given array has < 2 length', function() {
+    expect(binarySearch([4], 4)).toBe(true);
+  });
+
+  test('returns true if val is in the given array at the last index', function() {
+    console.log('fourth');
+    expect(binarySearch([1, 2, 3, 4, 5, 6], 6)).toBe(true);
+  });
+
+  test('returns true if val is in the given array at the first index', function() {
+    expect(binarySearch([1, 2, 3, 4, 5, 6], 1)).toBe(true);
+  });
+
+  test('returns false if val is not in the given array', function() {
     expect(binarySearch([1, 2, 3, 4, 5, 6], 10)).toBe(false);
   });
 
-  it('returns false if no array is given', function() {
+  test('returns false if no array is given', function() {
     expect(binarySearch(undefined, 1)).toBe(false);
   });
 
-  it('returns false if no value is given', function() {
+  test('returns false if no value is given', function() {
     expect(binarySearch([1, 2, 3], undefined)).toBe(false);
   });
 });
